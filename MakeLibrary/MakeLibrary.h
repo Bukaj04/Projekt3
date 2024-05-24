9d1edc42fa4bc4293302e62c2867d5efc90a917a
Bukaj04 <168406988+Bukaj04@users.noreply.github.com>
2024-05-24 22:30:20 +0200

    Delete MakeLibrary directory

--- MakeLibrary/MakeLibrary.h
+++
@@ -1,9 +0,0 @@
-﻿// MakeLibrary.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include <matplot/matplot.h>
-
-// TODO: Reference additional headers your program requires here.
